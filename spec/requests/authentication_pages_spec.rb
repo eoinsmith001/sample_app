0909f6daed79b1a6bf8a76d89e87398a48e6e1f5
Eoin Smith <eoinsmith001@gmail.com>
2014-04-12 14:54:01 +1000

    passing tests for basic signin page

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -1,8 +1,11 @@
 require 'spec_helper'
 
-describe "AuthenticationPages" do
+describe "Authentication" do
+
   subject {page}
+
   describe "signin page" do
+    before { visit signin_path }
     it {should have_content('Sign in')}
     it {should have_title('Sign in')}
   end
